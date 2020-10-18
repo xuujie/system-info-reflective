f8bdfe53e2602123694165dc9646123b59c1a3a5
xujie <xuujie@gmail.com>
2020-10-18 22:19:53 +0800

    add db ref

--- build.sh
+++
@@ -1,3 +0,0 @@
-gradle clean build
-docker build -t xuujie/system-info-reflective:latest .
-docker push xuujie/system-info-reflective:latest
---
+++ src/main/resources/schema.sql
@@ -0,0 +1,5 @@
+CREATE TABLE SYSTEM_HEALTH (
+  HOST varchar(100),
+  TIME_CHECKED timestamp,
+  constraint pk_host primary key (HOST)
+);
